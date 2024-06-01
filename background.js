d1a177252ec08931b952072f575269ddb449c492
LeoChen21 <leochenkmts@gmail.com>
2024-06-01 18:02:59 -0400

    auto

--- background.js
+++ background.js
@@ -1,9 +1,5 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/shorts")) {
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW"
-      });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "NEW") {
+        console.log("new video detected");
     }
-  });
-  
+});
--- content.js
+++ content.js
@@ -1,1 +1,15 @@
-console.log("This extension is running on a specified site.");
+// Function to detect new Shorts video elements
+function detectNewShortsVideos() {
+    chrome.tabs.onUpdated.addListener((tabId, tab) => {
+        if (tab.url && tab.url.includes("youtube.com/shorts")) {
+      
+          chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            videoId: urlParameters.get("v"),
+          });
+        }
+      });
+}
+
+// Start detecting new Shorts videos
+detectNewShortsVideos();
