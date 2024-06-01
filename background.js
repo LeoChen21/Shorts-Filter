a894c412d555b83eabf41f604500df87b3f9cf4b
LeoChen21 <leochenkmts@gmail.com>
2024-06-01 18:33:36 -0400

    auto

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "NEW") {
-        console.log("new video detected");
-    }
-});
+function detectNewShortsVideos() {
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+        console.log(tab.url, changeInfo.status === 'complete');
+        if (changeInfo.status === 'complete') {
+            const url = new URL(tab.url);
+            const videoId = url.pathname.split("/")[2]; // Extract video ID from the URL
+
+            chrome.tabs.sendMessage(tabId, {
+                type: "NEW",
+                videoId: videoId,
+            });
+            console.log("background.js detected new video.");
+        }
+    });
+}
+
+// Start detecting new Shorts videos
+detectNewShortsVideos();
--- content.js
+++ content.js
@@ -1,15 +1,6 @@
-// Function to detect new Shorts video elements
-function detectNewShortsVideos() {
-    chrome.tabs.onUpdated.addListener((tabId, tab) => {
-        if (tab.url && tab.url.includes("youtube.com/shorts")) {
-      
-          chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v"),
-          });
-        }
-      });
-}
-
-// Start detecting new Shorts videos
-detectNewShortsVideos();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === "NEW") {
+        console.log("New YouTube Shorts video detected:", message.videoId);
+        // Optionally, you could perform more actions here
+    }
+});
